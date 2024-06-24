0fa1b98c181f971232e4335c222435cb80193cc1
Walrus98 <samuele.calugi@gmail.com>
2024-06-24 18:59:44 +0200

    feat(fe): small refractoring

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { NextUIProvider } from '@nextui-org/react'
 import App from './App.tsx'
-import './index.css'
+import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
